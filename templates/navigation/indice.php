4848960d2528af982c50652401d28cc61ed15797
Unknown <estebancureno@hotmail.com>
2018-04-04 17:21:53 -0500

    HTML
    HTML

--- templates/navigation/indice.php
+++ templates/navigation/indice.php
@@ -1,10 +1,11 @@
 <ul>
 	<li class="collapsible open">
 	    <a href="#" class="icon-folder">Archivos</a>
-	    <ul>
+	    <ul class="conenidoArchivos">
 		<li><div class="app-navigation-entry-bullet"></div><a herf="#">HTML</a></li>
 		<li><a herf="#">CSS</a></li>
 		<li><a herf="#">JAVASCRIPT</a></li>
+        <li><a herf="#">GUARDAR ARCHIVO</a></li>
 	    </ul>
 	</li>
 </ul>
