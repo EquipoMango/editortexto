fd559fec422d2ff2e63b509e983bdc2e51056211
Unknown <estebancureno@hotmail.com>
2018-04-04 17:19:11 -0500

    js
    js

--- js/editor.js
+++ js/editor.js
@@ -3,4 +3,15 @@ $(document).ready(function(){
 	$("#result").empty();
         $("#result").append($("#source_code").val());
     });
+    $(".icon-folder").click(function(){
+        if($(".collapsible").hasClass("open")){
+                $(".contenidoArchivos").hide();
+                $(".collapsible").removeClass("open");            
+        }
+        else{
+                $(".contenidoArchivos").show();
+                $(".collapsible").addClass("open"); 
+        }    
+    } );    
+    
 });
